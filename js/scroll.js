4afaa1ac1526e3bb63b80d87327014ce9b5dbc7a
Andrea Vecchiato <vecchiato.websites@gmail.com>
2020-08-03 20:46:07 +0200

    responsive

--- js/scroll.js
+++ js/scroll.js
@@ -2,5 +2,8 @@ new fullpage('#fullpage', {
     autoScrolling: true,
     navigation: true,
     controlArrows: true,
-    slideNavigation: true
+    slideNavigation: true,
+    navigationTooltips: ['Home Page', 'Chi Sono', 'Html & Css', 'Bootstrap', 'Sass', 'I miei lavori', 'I miei lavori', 'Contattami'],
+    showActiveTooltip: true,
+    anchors:['section1', 'section2', 'section3', 'section4', 'section5', 'section6', 'section7', 'section8']
 });
